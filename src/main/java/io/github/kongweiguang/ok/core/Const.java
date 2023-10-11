fa520ce4de5eba9d4f7b897e7367d86054190ec1
kongweiguang <240524885@qq.com>
2023-10-11 19:39:56 +0800

    fix code

--- src/main/java/io/github/kongweiguang/ok/core/Const.java
+++ src/main/java/io/github/kongweiguang/ok/core/Const.java
@@ -8,16 +8,16 @@ package io.github.kongweiguang.ok.core;
 public interface Const {
     String http = "http";
     String https = "https";
-
     String _http = "http://";
     String _https = "https://";
 
     String ws = "ws";
     String wss = "wss";
+    String _ws = "ws://";
+    String _wss = "wss://";
+
 
     String localhost = "127.0.0.1";
-
     int port = 80;
-
 
 }
