4340913c9af054881194a2f3dd1b28511f2cb409
kongweiguang <240524885@qq.com>
2023-11-01 23:06:46 +0800

    fix code

--- src/main/java/io/github/kongweiguang/ok/core/UA.java
+++ src/main/java/io/github/kongweiguang/ok/core/UA.java
@@ -9,7 +9,7 @@ public interface UA {
   String v();
 
   enum Win implements UA {
-    chrome("User-Agent:Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36"),
+    chrome("Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36"),
 
 
     ;
@@ -28,7 +28,7 @@ public interface UA {
 
 
   enum Mac implements UA {
-    chrome("User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"),
+    chrome("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"),
 
     ;
 
@@ -45,7 +45,7 @@ public interface UA {
   }
 
   enum Ubuntu implements UA {
-    chrome("User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Ubuntu/11.10 Chromium/27.0.1453.93 Chrome/27.0.1453.93 Safari/537.36"),
+    chrome("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Ubuntu/11.10 Chromium/27.0.1453.93 Chrome/27.0.1453.93 Safari/537.36"),
 
     ;
 
@@ -62,7 +62,7 @@ public interface UA {
   }
 
   enum Android implements UA {
-    chrome("User-Agent:Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19"),
+    chrome("Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19"),
 
     ;
     private final String v;
@@ -79,7 +79,7 @@ public interface UA {
 
 
   enum IPhone implements UA {
-   chrome("User-Agent:Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) CriOS/27.0.1453.10 Mobile/10B350 Safari/8536.25"),
+   chrome("Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_4 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) CriOS/27.0.1453.10 Mobile/10B350 Safari/8536.25"),
 
     ;
     private final String v;
@@ -95,7 +95,7 @@ public interface UA {
   }
 
   enum IPad implements UA {
-    safari("User-Agent:Mozilla/5.0 (iPad; CPU OS 8_1_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B466 Safari/600.1.4"),
+    safari("Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3"),
 
     ;
     private final String v;
